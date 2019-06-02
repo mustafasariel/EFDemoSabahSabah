1bd979f4618c5788e224131c6082df27d1373b3a
mustafa sarıel <mustafasariel@hotmail.com>
2019-06-02 14:36:58 +0300

    fakedata

---
+++ NortwindDenemeleri/Program.cs
@@ -0,0 +1,45 @@
+﻿using CodeFirstNorthwind;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NortwindDenemeleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //fillProduct();
+        }
+
+        static void fillProduct()
+        {
+            using (var contex=new NorthwindContext())
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    contex.Products.Add(new Product
+                    {
+                        ProductName = FakeData.TextData.GetAlphabetical(5),
+                        UnitPrice = FakeData.NumberData.GetNumber(10, 100),
+                        UnitsInStock = (short)FakeData.NumberData.GetNumber(100),
+                        Discontinued = FakeData.BooleanData.GetBoolean(),
+                        ReorderLevel = (short)FakeData.NumberData.GetNumber(1, 5),
+                        UnitsOnOrder = (short)FakeData.NumberData.GetNumber(20)
+
+                    });
+                }
+
+                contex.SaveChanges();
+               
+
+            }
+        }
+
+
+    }
+}
